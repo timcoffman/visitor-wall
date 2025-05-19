7a2e8b07264756f7479ae7216e6baabda202138a
Tim Coffman <234244+timcoffman@users.noreply.github.com>
2025-05-19 16:45:18 -0500

    improve display of admin site for inscriptions

--- visitorapp/admin.py
+++ visitorapp/admin.py
@@ -4,8 +4,13 @@ from django.contrib import admin
 
 from .models import Wall, Visitor, Inscription
 
+class InscriptionAdmin(admin.ModelAdmin):
+    list_display = ["wall", "moderation_status", "text", "signature"]
+    ordering = ["wall", "moderation_status", "created_date"]
+
+
 admin.site.register(Wall)
 admin.site.register(Visitor)
-admin.site.register(Inscription)
+admin.site.register(Inscription, InscriptionAdmin)
 
 
