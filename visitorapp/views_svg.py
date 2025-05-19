e1d618ea87dc54688448467b4fd3625a4fbac753
Tim Coffman <234244+timcoffman@users.noreply.github.com>
2025-05-19 11:53:28 -0500

    deliver alternate SVG for Chrome browsers

--- visitorapp/views_svg.py
+++ visitorapp/views_svg.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+import re
+
 def draw_banner(request):
 	text = request.GET['text']
 	# banner font-size can range between 0.5 and 0.3
@@ -16,7 +18,8 @@ def draw_banner(request):
 		'bannerFontSize': f'{bannerFontSize:.2f}',
 		'bannerFill': bannerFill,
 	}
-	return render( request, "visitorapp/banner.svg", context, content_type='image/svg+xml' )
+	return render_svg( request, "visitorapp/banner.svg", context )
+
 
 def draw_ring(request):
 	text = request.GET['text']
@@ -34,4 +37,19 @@ def draw_ring(request):
 		'bannerFontSize': f'{bannerFontSize:.2f}',
 		'bannerFill': bannerFill,
 	}
-	return render( request, "visitorapp/ring.svg", context, content_type='image/svg+xml' )
+	return render_svg( request, "visitorapp/ring.svg", context )
+
+# Chrome:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36
+# Safari:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15
+# Firefox: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:137.0) Gecko/20100101 Firefox/137.0
+
+RE_CHROM_USER_AGENT = re.compile( r'(^|\s)Chrome/[.\d]+(\s|$)' )
+
+def render_svg( request, resource, context ):
+	if "HTTP_USER_AGENT" in request.META:
+		userAgent = request.META["HTTP_USER_AGENT"]
+		if RE_CHROM_USER_AGENT.search( userAgent ):
+			chromeResource = resource.replace( '.svg', '.chrome.svg' )
+			print( f'detected Chrome; rendering {chromeResource} instead of {resource}' )
+			resource = chromeResource
+	return render( request, resource, context, content_type='image/svg+xml' )
